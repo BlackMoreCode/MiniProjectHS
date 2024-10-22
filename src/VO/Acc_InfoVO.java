eeb1258f66381dfa2ace329268dc488c2a282ea2
BlackMoreCode <sehorim1993@gmail.com>
2024-10-22 17:49:00 +0900

    10-22 invVO REQUIRES revision.

--- src/VO/Acc_InfoVO.java
+++ src/VO/Acc_InfoVO.java
@@ -6,7 +6,7 @@ public class Acc_InfoVO {
     private String userId;
     private String userPw;
     private String userName;
-    private String userPhone;// 필요시 String (DB에서는 varchar 처리?)
+    private String userPhone;// DB에서 char(13)라는 고정 값을 쓰므로 JDBC에선 유연성을 위해 문자열 처리
     private Date joinDate;
     private final int authLv = 3; // 회원가입할 아이디는 소비자로 한정되므로 authLv을 3으로 final & 하드코딩 처리.
     //세터에서도 제거, 생성자에서도 제거.
@@ -65,7 +65,11 @@ public class Acc_InfoVO {
     }
 
     public void setUserPhone(String userPhone) {
-        this.userPhone = userPhone;
+        if (userPhone != null && userPhone.length() == 13) { //13자리 입력시 문제 없이 통과.
+            this.userPhone = userPhone;
+        } else {    // 데이터베이스와의 일관성을 위해 제약 조건 부과.
+            throw new IllegalArgumentException("연락처는 정확히 '-' 을 포함해 13자여야 합니다.");
+        }
     }
 
     public void setJoinDate(Date joinDate) {
