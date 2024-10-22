4507a8e50e407c7143b5c581be55dd1a37636fc6
BlackMoreCode <sehorim1993@gmail.com>
2024-10-22 17:19:00 +0900

    10-22 Initial VO changes.

--- src/VO/Acc_InfoVO.java
+++ src/VO/Acc_InfoVO.java
@@ -1,4 +1,78 @@
 package VO;
 
+import java.sql.Date;
+
 public class Acc_InfoVO {
+    private String userId;
+    private String userPw;
+    private String userName;
+    private String userPhone;// 필요시 String (DB에서는 varchar 처리?)
+    private Date joinDate;
+    private final int authLv = 3; // 회원가입할 아이디는 소비자로 한정되므로 authLv을 3으로 final & 하드코딩 처리.
+    //세터에서도 제거, 생성자에서도 제거.
+    private String storeId;
+
+    public Acc_InfoVO(String userId, String userPw, String userName, String userPhone, Date joinDate, int authLv, String storeId) {
+        this.userId = userId;
+        this.userPw = userPw;
+        this.userName = userName;
+        this.userPhone = userPhone;
+        this.joinDate = joinDate;
+        this.storeId = storeId;
+    }
+
+    public Acc_InfoVO() {
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserPw() {
+        return userPw;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public int getAuthLv() {
+        return authLv;
+    }
+
+    public String getStoreId() {
+        return storeId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setUserPw(String userPw) {
+        this.userPw = userPw;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public void setStoreId(String storeId) {
+        this.storeId = storeId;
+    }
 }
